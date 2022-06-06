479348f13bba1504f74ba24132615987699a2175
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-06-06 10:32:03 +0000

    Customize admin

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+"""boutique_wt URL Configuration for products app """
+
+
+from django.urls import path
+from . import views
+
+
+# urlpatterns = [
+#     path('products', views.index, name='home')
+# ]
