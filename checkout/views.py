aa1282b89e1247ca43c55994f834bdd3f6ce847a
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-06-10 16:23:00 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L993GJlq2jGOSsnMIaPUB6JkQ1vwjoDONbgWYlicJCqwDNWhLXQbzoWV8mf0LIpAlvztSzBNesqS4pL5VBZzjbH00kgTl7fjw',
+        'client_secret': 'sk_test_51L993GJlq2jGOSsnl277whoCiuqr8NZVTKc1VmyCx2w3BITtZX29lViQ60mMZQVyQCxCtn9h3iqFOeoHLj2Fnfdv00slRUYCNg',
     }
 
     return render(request, template, context)
