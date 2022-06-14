dd00fd9bffc53cabd5aa76b617e73e93ad44cf3f
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-06-14 12:38:48 +0000

    Completed stripe webhook

--- checkout/views.py
+++ checkout/views.py
@@ -48,7 +48,6 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
-            print("secret with pid==", request.POST.get('client_secret'))
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
