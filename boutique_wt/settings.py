9852657e10322f8906e6ca918d8c3037b834d08c
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-06-23 16:43:15 +0000

    Deployment Part 2

--- boutique_wt/settings.py
+++ boutique_wt/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-poly', 'localhost']
+ALLOWED_HOSTS = ['boutique-poly.herokuapp.com', 'localhost']
 
 
 # Application definition
